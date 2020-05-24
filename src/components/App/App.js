ee0c6bac0c93ed2575c53458a39af95423e5675b
Brent Gorwin <brent.gorwin@gmail.com>
2020-05-24 10:05:33 -0500

    Finished the project Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,46 +1,58 @@
 import React from 'react';
 import './App.css';
-
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
+console.log(process.env.REACT_APP_YELP_API_KEY);
 
-  const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-  };
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-  const businesses = [
-    business,
-    business,
-    business,
-    business,
-    business,
-    business
-  ];
+		this.state = {
+			businesses: []
+		};
+		this.searchYelp = this.searchYelp.bind(this);
+	}
 
-  class App extends React.Component {
-  searchYelp(term, location, sortBy){
-    console.log(`You are searching for  ${term}, ${location} and ${sortBy}`);
-  }
+	searchYelp(term, location, sortBy) {
+		Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+			this.setState({ businesses: businesses });
+		});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Restaurant Search!</h1>
-        <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={businesses}/>
-      </div>
-    );
-  }
-  
+	render() {
+		console.log(process.env);
+		return (
+			<div className="App">
+				<h1>Restaurant Search!</h1>
+				<SearchBar searchYelp={this.searchYelp} />
+				<BusinessList businesses={this.state.businesses} />
+			</div>
+		);
+	}
 }
 
 export default App;
+
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ];
