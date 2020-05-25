d6159790a46e8ee9629826b73fa96fa34b19acf1
Brent Gorwin <brent.gorwin@gmail.com>
2020-05-25 08:15:08 -0500

    Updates

---
+++ precache-manifest.52ae6d1f39e6e6eefe5f29c603bf4f4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe72f5e131451f4b8bf1dc7857bec4d",
+    "url": "/restaurant-search/index.html"
+  },
+  {
+    "revision": "0f05d5eaec5502219167",
+    "url": "/restaurant-search/static/css/main.76f6399d.chunk.css"
+  },
+  {
+    "revision": "3aa1e09772aeba1ff672",
+    "url": "/restaurant-search/static/js/2.f74321a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-search/static/js/2.f74321a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f05d5eaec5502219167",
+    "url": "/restaurant-search/static/js/main.be9a79cd.chunk.js"
+  },
+  {
+    "revision": "7a020f98a84a741dea84",
+    "url": "/restaurant-search/static/js/runtime-main.5ec253da.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/restaurant-search/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/restaurant-search/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
